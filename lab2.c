8f627a542114d1eb60d637d7de7ea9ac8e56d325
Ana Iacob <anaiacob484@gmail.com>
2024-03-06 19:13:15 +0200

    LaboratorPA2

--- lab2.c
+++ lab2.c
@@ -1,20 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+
+int function(int x)
+{
+    if(x==0)
+        return 0;
+    else
+        return function(x-2);
 }
 
 int main(){
 	int a=30, i=0;
-	
+
 	while (a>0){
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
 
